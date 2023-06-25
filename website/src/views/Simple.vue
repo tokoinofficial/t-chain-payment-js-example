122501a4c1966b9b4ca800f31d92bacf4994a2fd
Vu Truong <vu.truong.16193@gmail.com>
2023-06-25 14:45:43 +0700

    [FRON-703] fix: better UX for mode switch button

--- website/src/views/Simple.vue
+++ website/src/views/Simple.vue
@@ -38,8 +38,9 @@
                 </v-col>
                 <v-col cols="3">
                   <v-switch
-                    v-model="isSandboxed"
-                    label="Toggle Sandbox Mode"
+                    v-model="isProduction"
+                    inset
+                    class="mode-switch"
                   />
                 </v-col>
                 <v-col cols="8">
@@ -58,7 +59,7 @@
                     <p>
                       TnxHash:
                       <a
-                        :href="`https://${isSandboxed ? 'testnet.' : ''}bscscan.com/tx/${transaction_hash}`"
+                        :href="`https://${isProduction ? 'testnet.' : ''}bscscan.com/tx/${transaction_hash}`"
                         target="_blank"
                       >
                         <strong>{{ transaction_hash }}</strong>
@@ -123,8 +124,9 @@
                 </v-col>
                 <v-col cols="3">
                   <v-switch
-                    v-model="isSandboxed"
-                    label="Toggle Sandbox Mode"
+                    v-model="isProduction"
+                    inset
+                    class="mode-switch"
                   />
                 </v-col>
                 <v-col cols="8">
@@ -202,13 +204,15 @@ export default {
     isLoading: false,
     currency: 'USD',
     currencies: ['USD', 'IDR'],
-    isSandboxed: true,
+    isProduction: false,
   }),
   watch: {
-    isSandboxed(toggleState) {
+    isProduction(toggleState) {
       Payment.init({
-        api_key: toggleState ? '3e093592-3e0e-4a52-9601-ead49f794586' : 'public_api_key-a8a27441-8e20-4154-a0f7-004d6e38e633',
-        mode: toggleState ? 'sandbox' : 'production',
+        api_key: toggleState
+          ? 'public_api_key-a8a27441-8e20-4154-a0f7-004d6e38e633'
+          : '3e093592-3e0e-4a52-9601-ead49f794586',
+        mode: toggleState ? 'production' : 'sandbox',
       })
     },
   },
@@ -258,10 +262,12 @@ export default {
     },
   },
 
-  created() {
+  mounted() {
     Payment.init({
-      api_key: this.isSandboxed ? '3e093592-3e0e-4a52-9601-ead49f794586' : 'public_api_key-a8a27441-8e20-4154-a0f7-004d6e38e633',
-      mode: this.isSandboxed ? 'sandbox' : 'production',
+      api_key: this.isProduction
+        ? 'public_api_key-a8a27441-8e20-4154-a0f7-004d6e38e633'
+        : '3e093592-3e0e-4a52-9601-ead49f794586',
+      mode: this.isProduction ? 'production' : 'sandbox',
     })
   },
 }
@@ -276,14 +282,17 @@ export default {
   width: 100%;
   height: 100%;
 }
+
 .row {
   width: 100%;
   align-content: center;
   justify-content: center;
 }
+
 .v-dialog .v-card {
   width: 100%;
 }
+
 .progress {
   position: absolute;
   background: rgba(0, 0, 0, 0.3);
@@ -295,5 +304,40 @@ export default {
   justify-content: center;
   align-items: center;
   z-index: 1000;
+}
+
+.mode-switch.v-input--switch--inset:deep(.v-input--switch__track),
+.mode-switch.v-input--switch--inset:deep(.v-input--selection-controls__input) {
+  width: 96px;
+  transform: scaleY(1.1);
+}
+
+.mode-switch:deep(.v-input--switch__thumb) {
+  transform: scale(1.2);
+}
+
+.mode-switch.v-input--switch--inset:deep(.v-input--switch__track) {
+  display: flex;
+}
+
+.mode-switch.v-input.v-input--switch--inset:deep(.v-input--switch__track:after) {
+  content: 'Sandbox';
+  color: black;
+  font-size: 12px;
+  margin: auto 8px auto auto;
+  float: right;
+}
+
+.mode-switch.v-input.v-input--switch--inset.v-input--is-label-active.v-input--is-dirty:deep(.v-input--switch__track:after) {
+  content: 'Production';
+  color: white;
+  font-size: 12px;
+  margin: auto auto auto 8px;
+  float: left;
+}
+
+.v-application--is-ltr .mode-switch.v-input--switch--inset.v-input--is-dirty:deep(.v-input--selection-controls__ripple),
+.v-application--is-ltr .mode-switch.v-input--switch--inset.v-input--is-dirty:deep(.v-input--switch__thumb) {
+  transform: translate(68px, 0) !important;
 }
 </style>
